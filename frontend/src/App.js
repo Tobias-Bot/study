2d0e4501572ed8e1833fb7cd4c4c10da48f9e793
may-studio <61617269+Tobias-Bot@users.noreply.github.com>
2021-05-13 17:38:58 +0300

    login page design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,11 @@ const Button = withRouter(({ history }) => (
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      form: "",
+    };
+
+    this.getForm = this.getForm.bind(this);
   }
 
   componentDidMount() {}
@@ -35,14 +39,50 @@ class App extends React.Component {
     //   });
   }
 
+  getForm(form) {
+    this.setState({ form });
+  }
+
   render() {
     // let index = 118;
+    let form = this.state.form.form;
+    let formTitle = this.state.form.title;
 
     return (
       <BrowserRouter>
+        <div
+          className="modal fade"
+          id="LoginModal"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  {formTitle}
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">{form}</div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-primary">
+                  ok
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="wrapper">
           <Route path="/">
-            <LoginPage />
+            <LoginPage getForm={this.getForm} />
           </Route>
           <Route path="/other">
             <NavLink to="/main">Go to main</NavLink>
--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,11 +1,11 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import { BrowserRouter, NavLink, Route, withRouter } from "react-router-dom";
 
-import Main from "./components/Main.js";
+import Main from "./Main.js";
 
-import "./App.css";
-import "..styles/login.css";
+import "../App.css";
+import "../styles/login.css";
 
 const Button = withRouter(({ history }) => (
   <button
@@ -22,15 +22,95 @@ class LoginPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    this.getLoginForm = this.getLoginForm.bind(this);
+    this.getRegForm = this.getRegForm.bind(this);
   }
 
   componentDidMount() {}
 
-  render() {
+  /* form render funcs */
 
+  getLoginForm() {
+    let response = {};
+
+    response.form = (
+      <div>
+        <div className="inputTitle">Логин</div>
+        <input className="inputForm" type="text" placeholder="введите логин" />
+        <br />
+        <br />
+        <div className="inputTitle">Пароль</div>
+        <input
+          className="inputForm"
+          type="password"
+          placeholder="введите пароль"
+        />
+      </div>
+    );
+
+    response.title = "Авторизация";
+
+    this.props.getForm(response);
+  }
+
+  getRegForm() {
+    let response = {};
+
+    response.form = (
+      <div>
+        <div className="inputTitle">Логин</div>
+        <input
+          className="inputForm"
+          type="text"
+          placeholder="придумайте имя пользователя"
+        />
+        <br />
+        <br />
+        <div className="inputTitle">Пароль</div>
+        <input
+          className="inputForm"
+          type="password"
+          placeholder="придумайте пароль"
+        />
+        <br />
+        <br />
+        <input
+          className="inputForm"
+          type="password"
+          placeholder="подтверждение пароля"
+        />
+      </div>
+    );
+
+    response.title = "Регистрация";
+
+    this.props.getForm(response);
+  }
+
+  /* send request funcs */
+
+  
+
+  render() {
     return (
       <BrowserRouter>
-        <div>Hi login
+        <div
+          className="authBtn"
+          style={{ marginTop: "45vh" }}
+          data-bs-toggle="modal"
+          data-bs-target="#LoginModal"
+          onClick={this.getLoginForm}
+        >
+          войти
+        </div>
+        <div
+          className="authBtn"
+          data-bs-toggle="modal"
+          data-bs-target="#LoginModal"
+          onClick={this.getRegForm}
+        >
+          регистрация
         </div>
       </BrowserRouter>
     );
