bae1ea4e25b07bf09bd8a5df706893b4927e6bc1
may-studio <61617269+Tobias-Bot@users.noreply.github.com>
2021-05-25 12:21:22 +0300

    new db models

--- backend/userroom/models.py
+++ backend/userroom/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Room(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=500, default="")
+    bio = models.TextField(max_length=3000, blank=True)
+    users = models.PositiveIntegerField(default=1)
+    colors = models.CharField(max_length=17, default="|#9795EF|#F9C5D1|")
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Room.objects.create(user=instance, username=instance.username)
